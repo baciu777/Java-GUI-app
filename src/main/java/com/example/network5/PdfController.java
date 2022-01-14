7279e7fd565660fe321ecff2be2f3bb0b102e09a
baciu777 <72201455+baciu777@users.noreply.github.com>
2022-01-14 18:21:41 +0200

    we are good

--- src/main/java/com/example/network5/PdfController.java
+++ src/main/java/com/example/network5/PdfController.java
@@ -42,32 +42,38 @@ public class PdfController {
     @FXML
     public void initialize() {
 
+        path="D:/school-projects/java-projects/";
 
-        path = "C:/Users/ioana/Documents/GitHub/pdfs/first.pdf";
         NameField.setText("first.pdf");
 
     }
-    public void handleDirectory()
-    {
+
+    public void handleDirectory() {
         Stage stage = new Stage();
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setInitialDirectory(new File("src"));
         File selectedDirectory = directoryChooser.showDialog(stage);
+        try {
 
-        System.out.println(selectedDirectory.getAbsolutePath());
-        String s = selectedDirectory.getAbsolutePath();
-        s = s.replace('\\','/');
-        s = s + "/";
-        path = s + NameField.getText();
+
+
+            String s = selectedDirectory.getAbsolutePath();
+            s = s.replace('\\', '/');
+            s = s + "/";
+            path=s;
+
+        } catch (ValidationException e) {
+            MessageAlert.showErrorMessage(null, e.getMessage());
+        }
 
     }
+
     @FXML
 
-    public List<Message> getHistoryMess()
-    {
+    public List<Message> getHistoryMess() {
         LocalDateTime start = startDate.getValue().atStartOfDay();
-        LocalDateTime end = endDate.getValue().atTime(23,59);
-        Predicate<Message> p1 = n ->n.getDate().isBefore(end) && n.getDate().isAfter(start);
+        LocalDateTime end = endDate.getValue().atTime(23, 59);
+        Predicate<Message> p1 = n -> n.getDate().isBefore(end) && n.getDate().isAfter(start);
 
         List<Message> mess = userLogin.getMessages();
         List<Message> output = mess.stream()
@@ -75,18 +81,23 @@ public class PdfController {
                 .collect(Collectors.toList());
         return output;
     }
-    public void handleExecute()
-    {
-        try{
-            LocalDate dateS=startDate.getValue();
-            LocalDate dateE=endDate.getValue();
 
-            if(!path.endsWith(".pdf"))
-                throw new ValidationException("Please select a pdf type of file");
-            if(dateS.isAfter(LocalDate.now()))
+    public void handleExecute() {
+        try {
+            LocalDate dateS = startDate.getValue();
+            LocalDate dateE = endDate.getValue();
+
+            if(NameField==null)
+                throw new ValidationException("select a name for the export file");
+            if(!NameField.getText().endsWith(".pdf"))
+                throw new ValidationException("select a pdf type");
+            if (dateS == null)
+                throw new ValidationException("the starting date is null");
+            if (dateS.isAfter(LocalDate.now()))
                 throw new ValidationException("Starting date must not be in the future");
-
-            if(dateE.isAfter(LocalDate.now()))
+            if (dateE == null)
+                throw new ValidationException("the ending date is null");
+            if (dateE.isAfter(LocalDate.now()))
                 throw new ValidationException("Ending date must not be in the future");
 
             PDDocument document = new PDDocument();
@@ -111,8 +122,7 @@ public class PdfController {
             contentStream.showText("Prieteni:");
             contentStream.endText();
             indexline = indexline - 30;
-            for(User x: friends)
-            {
+            for (User x : friends) {
                 contentStream.beginText();
                 contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
                 contentStream.newLineAtOffset(120, indexline);
@@ -127,8 +137,7 @@ public class PdfController {
             contentStream.showText("Mesaje:");
             contentStream.endText();
             indexline = indexline - 30;
-            for(Message x: getHistoryMess())
-            {
+            for (Message x : getHistoryMess()) {
                 contentStream.beginText();
                 contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
                 contentStream.newLineAtOffset(120, indexline);
@@ -140,20 +149,17 @@ public class PdfController {
             contentStream.close();
 
             System.out.println("Content added");
-
+            path =path+ NameField.getText();
             document.save(path);
 
 
             System.out.println("PDF created");
 
 
-
             document.close();
-        }catch (ValidationException e)
-        {
-            MessageAlert.showErrorMessage(null,e.getMessage());
-        }
-        catch (IOException e) {
+        } catch (ValidationException e) {
+            MessageAlert.showErrorMessage(null, e.getMessage());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
